526ca59b5dfa5aa2743c96353d495e7aa4f0bdd0
Ethan <ethan.macka@gmail.com>
2021-11-12 13:10:54 -0500

    finished assignment, now time to check for validation

---
+++ soundboard.js
@@ -0,0 +1,15 @@
+function playsound() {
+    var sound1 = document.getElementById("sound1");
+    sound1.autoplay = 'true';
+    sound1.load();
+}
+function playsound2() {
+    var sound1 = document.getElementById("sound2");
+    sound1.autoplay = 'true';
+    sound1.load();
+}
+function playsound3() {
+    var sound1 = document.getElementById("sound3");
+    sound1.autoplay = 'true';
+    sound1.load();
+}
